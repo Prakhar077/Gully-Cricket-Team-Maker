a26cca52b2403df753d9d0fec21eeea75d3e56ec
Prakhar077 <prakharsharma522@gmail.com>
2025-06-19 04:16:57 -0700

    Initial  commit 14

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -4,16 +4,16 @@ const pool = require('../db');
 exports.createAdmin = async (req, res) => {
   const { username, password } = req.body;
   const hashed = await bcrypt.hash(password, 10);
-  await pool.query('INSERT INTO admins (username, password) VALUES ($1, $2)', [username, hashed]);
+  await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashed]);
   res.json({ message: 'Admin created' });
 };
 
 exports.getAdmins = async (req, res) => {
-  const result = await pool.query('SELECT * FROM admins');
+  const result = await pool.query('SELECT * FROM users');
   res.json(result.rows);
 };
 
 exports.deleteAdmin = async (req, res) => {
-  await pool.query('DELETE FROM admins WHERE id = $1', [req.params.id]);
+  await pool.query('DELETE FROM users WHERE id = $1', [req.params.id]);
   res.json({ message: 'Deleted' });
 };
